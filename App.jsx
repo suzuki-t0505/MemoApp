2a151bc4d25c8b6e7b4f0f884c881778235437e4
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2023-08-17 21:52:22 +0900

    ✨: Create MemoEdiitScrteen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
 
 const App = () => (
-  <MemoListScreen />
+  // <MemoListScreen />
   // <MemoDetailScreen />
+  // <MemoEditScreen />
+  <MemoCreateScreen />
 );
 
 export default App;
---
+++ src/screens/MemoEditScreen.jsx
@@ -0,0 +1,37 @@
+import {
+  KeyboardAvoidingView, StyleSheet, TextInput, View,
+} from 'react-native';
+import React from 'react';
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+const MemoEditScreen = () => (
+  <KeyboardAvoidingView style={styles.container} behavior="height">
+    <AppBar />
+
+    <View style={styles.inputContainer}>
+      <TextInput value="買い物リスト" multiline style={styles.input} />
+    </View>
+
+    <CircleButton name="check" />
+  </KeyboardAvoidingView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 1,
+    paddingHorizontal: 27,
+    paddingVertical: 32,
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
+
+export default MemoEditScreen;
